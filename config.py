3f43f598e0f41c9b55f9ddcdcc8f9b53ff6e4489
bishalkumar000001 <bishalkumar000001@gmail.com>
2026-09-03 04:54:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,8 +57,8 @@ class Config:
         self.VIDEO_MAX_HEIGHT: int = self._parse_video_height()
 
         # ArtistBots API @ArtistApibot
-        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "")
-        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "")
+        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "https://apiiiiii-d83edaacdabe.herokuapp.com")
+        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "jx_live_WKXBXuX3ZJ6wdEtNyf5-LTGk0ObbE2TxmxkabNmAcXqGCaCP")
         self.ENABLE_API: bool = self._str_to_bool(getenv("ENABLE_API", "True"))
         self.ENABLE_COOKIES_FALLBACK: bool = self._str_to_bool(getenv("ENABLE_COOKIES_FALLBACK", "True"))
         self.API_TIMEOUT: int = int(getenv("API_TIMEOUT", "60"))
