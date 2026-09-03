4eec548d84afa5fcd30672b82593f71127c4f726
bishalkumar000001 <bishalkumar000001@gmail.com>
2026-09-03 19:53:28 +0530

    Add files via upload

--- ArtistMusic/plugins/autoplay.py
+++ ArtistMusic/plugins/autoplay.py
@@ -1,14 +1,14 @@
 # ==========================================================
-  # Copyright (c) 2026 ArtistBots
+  # Copyright (c) 2026 VelocityBots
   # All Rights Reserved.
   #
-  # Project      : ArtistBots API Telegram Music Bot
-  # Powered By   : Artist
+  # Project      : VelocityBots API Telegram Music Bot
+  # Powered By   : ⎯꯭̽𓆩꯭͈〬𝐉͢αη𝐡νί ✗ Μυδί𝛓꯭ ̽🤍͢
   # Type         : API Based Telegram Music Bot
   #
-  # Bot          : @ArtistApibot
-  # Channel      : https://t.me/artistbots
-  # GitHub       : https://github.com/elevenyts/ArtistMusic
+  # Bot          : @JanhvixmusicRobot
+  # Channel      : https://t.me/VelocityBots
+  # GitHub       : https://github.com/bishalkumar000001/ArtistMusic
   #
   # Unauthorized copying, modification, or redistribution
   # of this source code without permission is prohibited.
